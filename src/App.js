9ac19f6dacb10e7941b1dd89abd26eb0d3edd1e7
hoummani <yassinehoummani.tsi@gmail.com>
2020-09-10 23:18:17 +0100

    Sign In Page is completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import { SignInPage } from './components/SignInPage';
 
 function App() {
   return (
     <div className="App">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Button
-      </button>
+      <SignInPage />
     </div>
   );
 }
